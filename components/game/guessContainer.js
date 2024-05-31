2e75bbf7374c0a51eca971cc7842a719607130d8
DiMNDev <Dimndev@gmail.com>
2024-05-30 20:55:37 -0600

    add GuessContainer

--- components/game/guessContainer.js
+++ components/game/guessContainer.js
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
 
 function GuessContainer({ children }) {
   return (
@@ -9,8 +10,21 @@ function GuessContainer({ children }) {
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  text: {},
+  container: {
+    width: 100,
+    marginVertical: 24,
+    backgroundColor: Colors.primary600,
+    borderRadius: 16,
+    borderColor: Colors.primary700,
+    borderWidth: 4,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.accent500,
+    textAlign: "center",
+    padding: 12,
+  },
 });
 
 export default GuessContainer;
