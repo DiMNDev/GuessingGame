cdea1ccd8b80551ac99256d81be07b46e01c3556
DiMNDev <Dimndev@gmail.com>
2024-05-30 20:55:56 -0600

    update GameScreen styling

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Title from "../components/ui/Title";
+import GuessContainer from "../components/game/guessContainer";
+import Colors from "../constants/colors";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -19,8 +21,17 @@ function GameScreen(userNumber) {
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      <View>
-        <Text>Higher or Lower?</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>Higher or Lower?</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>+</Text>
+        </View>
+        <GuessContainer>69</GuessContainer>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>-</Text>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
@@ -29,8 +40,45 @@ function GameScreen(userNumber) {
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
     padding: 24,
+  },
+  infoContainer: {
+    margin: 14,
+    padding: 14,
+    backgroundColor: Colors.primary600,
+    borderColor: Colors.primary700,
+    borderWidth: 4,
+    borderRadius: 16,
+  },
+  infoText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: 300,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 10,
+    width: 100,
+    padding: 4,
+    backgroundColor: Colors.primary600,
+    borderColor: Colors.primary700,
+    borderWidth: 4,
+    borderRadius: 16,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 48,
   },
 });
 
