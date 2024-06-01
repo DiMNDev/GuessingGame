49265bd1f38bb461be741640149c44bc933bf06a
DiMNDev <Dimndev@gmail.com>
2024-05-31 19:58:54 -0600

    add fonts and splashscreen packages

--- App.js
+++ App.js
@@ -1,22 +1,81 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(null);
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [rounds, setRounds] = useState(0);
+  const [roundData, setRoundData] = useState([{}]);
+
+  const [fontsLoaded, fontsError] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontsError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
+
+  if (!fontsLoaded && !fontsError) {
+    return null;
+  }
 
   function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
+    setUserNumber(parseInt(pickedNumber));
+    setGameIsOver(false);
+  }
+
+  function incrementRoundsHandler() {
+    setRounds((rounds) => (rounds += 1));
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true);
+  }
+
+  function getGuessRoundsdata(rounds) {
+    setRoundData(rounds);
   }
 
   let screen = <StartGameScreen setPickedNumber={pickedNumberHandler} />;
 
-  if (userNumber) {
-    screen = <GameScreen />;
+  function resetGame() {
+    setUserNumber(null);
+    setRounds(0);
+    screen = <StartGameScreen setPickedNumber={pickedNumberHandler} />;
   }
+
+  if (userNumber) {
+    screen = (
+      <GameScreen
+        userNumber={userNumber}
+        gameOver={gameOverHandler}
+        incrementRounds={incrementRoundsHandler}
+        roundsData={getGuessRoundsdata}
+      />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        rounds={rounds}
+        roundData={roundData}
+        userNumber={userNumber}
+        resetGame={resetGame}
+      />
+    );
+  }
+
   return (
     <LinearGradient
       colors={[Colors.accent500, Colors.primary700]}
@@ -28,7 +87,9 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        <SafeAreaView style={styles.rootScreen} onLayout={onLayoutRootView}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
