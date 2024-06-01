3bcf427d2e833f9dc4424350bf919b145b0fb12e
DiMNDev <Dimndev@gmail.com>
2024-05-31 22:18:54 -0600

    use FlatList

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert, FlatList } from "react-native";
 import Title from "../components/ui/Title";
 import GuessContainer from "../components/game/guessContainer";
 import Colors from "../constants/colors";
 import GuessButton from "../components/ui/GuessButton";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import RoundCard from "../components/ui/RoundCard";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -73,9 +74,6 @@ function GameScreen({ userNumber, gameOver, incrementRounds, roundsData }) {
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoText}>Higher or Lower?</Text>
-      </View>
       <View style={styles.buttonsContainer}>
         <GuessButton callback={nextGuessHandler.bind(this, "higher")}>
           <Ionicons name="add" size={48} />
@@ -85,19 +83,42 @@ function GameScreen({ userNumber, gameOver, incrementRounds, roundsData }) {
           <Ionicons name="remove" size={48} />
         </GuessButton>
       </View>
-      {/* <View>LOG ROUNDS</View> */}
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>Higher or Lower?</Text>
+      </View>
+      <FlatList
+        style={{
+          marginTop: 14,
+          marginBottom: 0,
+          alignSelf: "stretch",
+        }}
+        data={guessRounds}
+        renderItem={({ item, index }) =>
+          index < guessRounds.length - 1 ? (
+            <RoundCard
+              key={index}
+              guess={item.guess}
+              hol={item.hol}
+              index={index - guessRounds.length + 1}
+            />
+          ) : (
+            ""
+          )
+        }
+        keyExtractor={(item, index) => index}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    padding: 24,
   },
   infoContainer: {
-    margin: 14,
+    marginTop: 14,
     padding: 14,
     backgroundColor: Colors.primary600,
     borderColor: Colors.primary700,
@@ -113,6 +134,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+    marginVertical: 24,
   },
 });
 
