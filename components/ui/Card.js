9ccae1f17c6cbe608838b6abdb20828a5943b317
DiMNDev <Dimndev@gmail.com>
2024-05-31 19:59:35 -0600

    add Card component

---
+++ components/ui/Card.js
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+function Card({ children, style }) {
+  return <View style={[styles.card, style]}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary600,
+    borderRadius: 16,
+    borderColor: Colors.primary700,
+    borderWidth: 4,
+    borderRadius: 16,
+    // Android
+    elevation: 8,
+    // iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
+
+export default Card;
