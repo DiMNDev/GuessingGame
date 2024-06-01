a34a658e2ce1ad2ba21aa26733686ca211e71516
DiMNDev <Dimndev@gmail.com>
2024-06-01 10:10:15 -0600

    made Title responsive

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,19 +1,22 @@
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 import Colors from "../../constants/colors";
+import { useState } from "react";
 
 function Title({ children, containerStyle, textStyle }) {
+  const { width, height } = useWindowDimensions();
+  const titleFlex = height < 500 ? { flex: 1 } : { flex: 0 };
   return (
-    <View style={[styles.titleContainer, containerStyle]}>
+    <View style={[styles.titleContainer, titleFlex, containerStyle]}>
       <Text style={[styles.title, textStyle]}>{children}</Text>
     </View>
   );
 }
 
-const deviceHeight = Dimensions.get("window").height;
-
 const styles = StyleSheet.create({
   titleContainer: {
-    marginTop: deviceHeight * 0.05,
+    marginHorizontal: 24,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.primary600,
     borderRadius: 16,
     borderColor: Colors.primary700,
